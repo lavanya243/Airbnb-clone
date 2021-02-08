aa40cb702717e3e518732059452d1b021fa7e7ae
Lavanya <lavanya243@outlook.com>
2021-02-09 00:15:17 +0530

    First commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+function Footer() {
+    return (
+        <div className='footer'>
+         <p>© 2021 Airbnb Clone - No rights reserved - This is a demo </p>   
+         <br></br>
+         <p>Privacy  •  Terms & Conditions  •  Sitemap  •  Help  </p>
+        </div>
+    )
+}
+
+export default Footer;
